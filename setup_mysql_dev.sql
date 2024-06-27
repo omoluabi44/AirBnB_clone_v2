2d5ab622e73e4f5ba435eb20c3762d4eae11e0c4
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-06-27 20:24:33 +0100

    script for creating users

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,15 @@
+-- setup_mysql_dev.sql
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create the user if it doesn't exist and set the password
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY '${hbnb_dev_pwd}';
+
+-- Grant all privileges on the database to the user
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege on performance_schema to the user
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Flush the privileges to ensure all changes are applied
+FLUSH PRIVILEGES;
