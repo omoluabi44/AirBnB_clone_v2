2c3a998068a881edae61d2cf760167bd31e95d5e
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-06-27 20:41:09 +0100

    bug fixed

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,7 +3,7 @@
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
 -- Create the user if it doesn't exist and set the password
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY '${hbnb_dev_pwd}';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Grant all privileges on the database to the user
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
