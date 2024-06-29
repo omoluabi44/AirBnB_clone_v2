37675fcfb6029347537871a8f4b09f7087be89c5
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-06-29 15:08:35 +0100

    fixed bug

--- models/place.py
+++ models/place.py
@@ -18,6 +18,6 @@ class Place(BaseModel, Base):
     number_bathrooms = Column(Integer, default=0, nullable=False)
     max_guest = Column(Integer, default=0, nullable=False)
     price_by_night = Column(Integer, default=0, nullable=False)
-    latitude = Column(Float, default=0, nullable=True)
-    longitude = Column(Float, default=0, nullable=True)
+    latitude = Column(Float)
+    longitude = Column(Float)
     amenity_ids = []
