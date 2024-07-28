a6904e1c6f585c81972bf4855490fecddb53c9ad
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-07-28 21:11:21 +0100

    task 9

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """display a HTML page with the states listed in alphabetical order"""
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    states = sorted(list(storage.all(State).values()), key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
 
 
